06c610627d02af75c234902e6bf42a33d23576fe
754119201@qq.com <754119201@qq.com>
2018-12-14 19:47:09 +0800

    初次提交文件

---
+++ app/src/main/java/com/example/track/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.track;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;9
+
+public class +MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
